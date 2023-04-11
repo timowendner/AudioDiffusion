6a5fda820d6b95717bdcd07938e53bddff7ad3a7
Timo Wendner <timo.wendner@gmx.net>
2023-04-11 21:17:14 +0200

    bugfix

--- dataloader.py
+++ dataloader.py
@@ -60,7 +60,8 @@ class AudioDataset(Dataset):
         # noise = (torch.rand(waveform.shape,)
         #          * 2 - 1) * torch.clamp(r - 0.5, min=0)
         # target = waveform + noise
-
+        model_input = waveform
+        target = waveform
         model_input = model_input * 0.98 / torch.max(model_input)
         target = target * 0.98 / torch.max(target)
 
