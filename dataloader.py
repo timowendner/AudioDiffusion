3232607a37c13956b83bb570bb88ec0e50d32e4d
Timo Wendner <timo.wendner@gmx.net>
2023-04-11 21:16:17 +0200

    remove diffusion and eq

--- dataloader.py
+++ dataloader.py
@@ -35,16 +35,16 @@ class AudioDataset(Dataset):
         eq_probability = 0.3
 
         # Apply EQ
-        freq_bands = (20, 60, 100, 250, 400, 630, 900, 1200, 1600, 2000, 2500,
-                      3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000)
-        if torch.rand(1,) < eq_probability:
-            waveform = F.equalizer_biquad(
-                waveform,
-                sr,
-                center_freq=np.random.choice(freq_bands),
-                gain=np.random.uniform(-6, 6),
-                Q=0.707
-            )
+        # freq_bands = (20, 60, 100, 250, 400, 630, 900, 1200, 1600, 2000, 2500,
+        #               3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000)
+        # if torch.rand(1,) < eq_probability:
+        #     waveform = F.equalizer_biquad(
+        #         waveform,
+        #         sr,
+        #         center_freq=np.random.choice(freq_bands),
+        #         gain=np.random.uniform(-6, 6),
+        #         Q=0.707
+        #     )
 
         # Apply gain
         # waveform = waveform * (1 - torch.rand(1,) ** 4)
@@ -54,12 +54,12 @@ class AudioDataset(Dataset):
         waveform = torch.roll(waveform, int(start))
 
         # create the diffusion
-        r = (torch.rand(1,) ** 2) * 0.0
-        noise = (torch.rand(waveform.shape,) * 2 - 1) * r
-        model_input = waveform + noise
-        noise = (torch.rand(waveform.shape,)
-                 * 2 - 1) * torch.clamp(r - 0.5, min=0)
-        target = waveform + noise
+        # r = (torch.rand(1,) ** 2) * 0.0
+        # noise = (torch.rand(waveform.shape,) * 2 - 1) * r
+        # model_input = waveform + noise
+        # noise = (torch.rand(waveform.shape,)
+        #          * 2 - 1) * torch.clamp(r - 0.5, min=0)
+        # target = waveform + noise
 
         model_input = model_input * 0.98 / torch.max(model_input)
         target = target * 0.98 / torch.max(target)
