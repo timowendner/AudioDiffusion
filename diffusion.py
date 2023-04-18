a3251c71305fe7cb2df8f0a89edcd7d1c295d682
Timo Wendner <timo.wendner@gmx.net>
2023-04-19 01:12:41 +0200

    device

--- diffusion.py
+++ diffusion.py
@@ -40,7 +40,7 @@ class Diffusion(nn.Module):
 
             # loop through all timesteps
             for i in reversed(range(1, self.steps)):
-                t = (torch.ones(n) * i).long()
+                t = (torch.ones(n) * i).long().to(self.model.device)
                 alpha = self.alpha[t]
                 alpha_hat = self.alpha_hat[t]
                 beta = self.beta[t]
