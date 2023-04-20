996a6e2c4cf479018b28f991f251158d022181cf
Timo Wendner <timo.wendner@gmx.net>
2023-04-20 10:53:31 +0200

    test

--- model.py
+++ model.py
@@ -75,8 +75,6 @@ class UNet(nn.Module):
         )
 
     def forward(self, x: Tensor, timestamp: Tensor, label: Tensor) -> Tensor:
-        timestamp = timestamp.int()
-        label = label.int()
 
         t1 = self.sinusoidal(88200, timestamp)
         t2 = self.sinusoidal(22050, timestamp)
