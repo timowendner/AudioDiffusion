6db7050db1a0b085320ff6b81dba006cd66e3486
Timo Wendner <timo.wendner@gmx.net>
2023-04-20 11:08:23 +0200

    test

--- model.py
+++ model.py
@@ -103,8 +103,10 @@ class UNet(nn.Module):
         x3 = self.down3(torch.cat([t3, l3, self.pool(x2)], 1))
         x4 = self.down4(torch.cat([t4, l4, self.pool(x3)], 1))
 
-        step_embedding = self.step_embedding(timestamp)
+        step_embedding = self.step_embedding(timestamp).view()
+        step_embedding = torch.unsqueeze(step_embedding, 1)
         label_embedding = self.label_embedding(label)
+        label_embedding = torch.unsqueeze(label_embedding, 1)
         print(step_embedding.shape)
         print(label_embedding.shape)
         out = torch.cat(
