d57145af3a2b215a4f21d2c121ea4ca8b45c48cf
Timo Wendner <timo.wendner@gmx.net>
2023-04-22 00:56:24 +0200

    model2

--- model2.py
+++ model2.py
@@ -117,6 +117,8 @@ class UNet(nn.Module):
         x3 = self.down3(torch.cat([t3, l3, self.pool(x2)], 1))
         x4 = self.down4(torch.cat([t4, l4, self.pool(x3)], 1))
 
+        print(timestamp.shape, timestamp.dtype)
+
         step_embedding = self.step_embedding(timestamp)
         label_embedding = self.label_embedding(label)
         print(step_embedding.shape, label_embedding.shape)
