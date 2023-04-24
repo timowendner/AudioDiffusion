8e20a3005f273fee47ada9c113de405cae21dab7
Timo Wendner <timo.wendner@gmx.net>
2023-04-24 20:00:34 +0200

    model1

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 import numpy as np
 from torch import sin, cos, pow, Tensor
+import torch.nn.functional as F
 
 
 class Sinusoidal(nn.Module):
@@ -9,18 +10,15 @@ class Sinusoidal(nn.Module):
         super(Sinusoidal, self).__init__()
         self.device = device
 
-    def forward(self, length: int, position: torch.Tensor) -> torch.Tensor:
+    def forward(self, position: torch.Tensor, length: int) -> torch.Tensor:
         n = position.shape[0]
-        values = torch.arange(length, device=self.device).unsqueeze(
-            0).unsqueeze(0).expand(n, 1, -1)
-        output = torch.zeros_like(values, device=self.device)
-        position = position.view(-1, 1, 1)
-
-        output[:, :, ::2] = sin(
-            position / pow(1000, values[:, :, ::2] / length))
-        output[:, :, 1::2] = cos(
-            position / pow(1000, values[:, :, 1::2] / length))
-        return output
+        values = torch.arange(length, device=self.device)
+        output = torch.zeros((n, length), device=self.device)
+        output[:, ::2] = sin(position.view(-1, 1) /
+                             pow(1000, values[::2] / length))
+        output[:, 1::2] = cos(position.view(-1, 1) /
+                              pow(1000, values[1::2] / length))
+        return output.view(n, 1, -1)
 
 
 def dual(in_channel, out_channel):
@@ -60,24 +58,28 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
         self.device = device
         self.sinusoidal = Sinusoidal(device)
+        self.step_count = step_count
+        self.label_count = label_count
 
-        self.down1 = dual(1, 48)
-        self.down2 = dual(48, 96)
-        self.down3 = dual(96, 192)
-        self.down4 = dual(192, 384)
+        c1, c2, c3, c4 = 8, 16, 32, 64
+
+        self.down1 = dual(1, c1)
+        self.down2 = dual(c1, c2)
+        self.down3 = dual(c2, c3)
+        self.down4 = dual(c3, c4)
 
         self.pool = nn.MaxPool1d(kernel_size=4, stride=4)
 
         self.step_embedding = embedding(step_count, 344)
         self.label_embedding = embedding(label_count, 344)
 
-        self.up4 = up(384 + 2, 384, pad=2)
-        self.up3 = up(768, 192)
-        self.up2 = up(384, 96, pad=2)
-        self.up1 = up(192, 48)
+        self.up4 = up(c4 + 2, c4, pad=2)
+        self.up3 = up(c4*2, c3)
+        self.up2 = up(c3*2, c2, pad=2)
+        self.up1 = up(c2*2, c1)
 
         self.output = nn.Sequential(
-            nn.Conv1d(96 + 2, 64, kernel_size=9, padding=4),
+            nn.Conv1d(c1*2 + 2, 64, kernel_size=9, padding=4),
             nn.ReLU(inplace=True),
             nn.Conv1d(64, 64, kernel_size=9, padding=4),
             nn.ReLU(inplace=True),
@@ -85,33 +87,37 @@ class UNet(nn.Module):
         )
 
     def forward(self, x: Tensor, timestamp: Tensor, label: Tensor) -> Tensor:
+        timestamp = timestamp.to(self.device)
+        label = label.to(self.device)
+        n = x.shape[0]
 
-        t1 = self.sinusoidal(88200, timestamp)
-        t2 = self.sinusoidal(22050, timestamp)
-        t3 = self.sinusoidal(5512, timestamp)
-        t4 = self.sinusoidal(1378, timestamp)
-        t5 = self.sinusoidal(344, timestamp)
+        t1 = self.sinusoidal(timestamp, 88200)
+        t2 = self.sinusoidal(timestamp, 22050)
+        t3 = self.sinusoidal(timestamp, 5512)
+        t4 = self.sinusoidal(timestamp, 1378)
+        t5 = self.sinusoidal(timestamp, 344)
 
-        l1 = self.sinusoidal(88200, label)
-        l2 = self.sinusoidal(22050, label)
-        l3 = self.sinusoidal(5512, label)
-        l4 = self.sinusoidal(1378, label)
-        l5 = self.sinusoidal(344, label)
+        l = label * 100
+        l1 = self.sinusoidal(l, 88200)
+        l2 = self.sinusoidal(l, 22050)
+        l3 = self.sinusoidal(l, 5512)
+        l4 = self.sinusoidal(l, 1378)
+        l5 = self.sinusoidal(l, 344)
+
+        timestamp = F.one_hot(timestamp.long(), self.step_count + 1)
+        timestamp = timestamp.view(n, 1, -1).type(torch.float32)
+        label = F.one_hot(label.long(), self.label_count + 1)
+        label = label.view(n, 1, -1).type(torch.float32)
 
         x = torch.cat([t1, l1, x], 1)
-        print(x.shape, x.dtype)
-
         x1 = self.down1(x)
         x2 = self.down2(torch.cat([t2, l2, self.pool(x1)], 1))
         x3 = self.down3(torch.cat([t3, l3, self.pool(x2)], 1))
         x4 = self.down4(torch.cat([t4, l4, self.pool(x3)], 1))
 
-        step_embedding = self.step_embedding(timestamp)
-        step_embedding = torch.unsqueeze(step_embedding, 1)
-        label_embedding = self.label_embedding(label)
-        label_embedding = torch.unsqueeze(label_embedding, 1)
-        out = torch.cat(
-            [t5, l5, step_embedding, label_embedding, self.pool(x4)], 1)
+        step_emb = self.step_embedding(timestamp)
+        label_emb = self.label_embedding(label)
+        out = torch.cat([t5, l5, step_emb, label_emb, self.pool(x4)], 1)
 
         out = self.up4(out)
         out = self.up3(torch.cat([t4, l4, out, x4], 1))
